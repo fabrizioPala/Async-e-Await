d00aab84b85316ee47222fa11fb1cef2262791af
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2023-12-06 20:58:47 +0100

    Add files via upload

---
+++ Esercizio1.js
@@ -0,0 +1,35 @@
+/*Riscrivi la funzione fetchDataFromAPI dell'esercizio Le Callback - 
+6 utilizzando Promises per una migliore gestione degli errori.
+La promise dovrebbe risolversi con i dati e rifiutare con un messaggio di errore.
+function fetchDataFromAPI() {
+  //....
+}
+
+fetchDataFromAPI()
+*/
+
+let obj = {
+  name: "Jhon",
+  age: 30
+};
+
+function fetchDataFromAPI(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Errore: dati non disponibili");
+      }
+    }, 1000);
+  });
+}
+
+fetchDataFromAPI(obj)
+  .then((result) => {
+    console.log("Dati ricevuti:", result);
+  })
+  .catch((error) => {
+    console.error("Errore durante il recupero dei dati:", error);
+  });
+
